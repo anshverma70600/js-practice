0066883ef96ed49d2fc48e4b32e4e42ecfc3048a
Ansh Verma <97027597+anshverma70600@users.noreply.github.com>
2024-02-21 18:25:02 +0000

    container config done

---
+++ basics/01_variable.js
@@ -0,0 +1,14 @@
+const userId = 123546;
+let userEmail = "hello@gmail.com"
+var userPwd = "14731f67re"
+userCity = "Indore"
+
+// now i will try to change all this variables and constant
+// userId = 2 (Not allowed)
+
+userEmail = "anfnoia@gmail.com"
+userPwd = "3582"
+userCity = "Jaipur"
+console.log(userId);
+console.table([userId, userEmail,userPwd, userCity])
+
