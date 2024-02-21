b48b29cb2310d07dbf51647b48222734cca22735
Ansh Verma <97027597+anshverma70600@users.noreply.github.com>
2024-02-21 19:35:29 +0000

    summary

---
+++ basics/04_summary.js
@@ -0,0 +1,17 @@
+// primitive 
+// 7types ==> String, number, boolean, null, undefined, symbol, bigint
+// let id = Symbol('1234')
+// let id2 = Symbol('1234')
+// console.log(id===id2);
+
+//Reference type || Non-primitive
+// Array, Object, Functions
+
+const cars = ["tata","maruti","honda"];
+const myobj ={
+    name : "Tata Tiago",
+    age : 1,
+}
+
+console.log(cars);
+console.log(myobj);
