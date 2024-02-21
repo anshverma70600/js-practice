630f25dbeb5d9741e5534833ff0fea1f5a23a155
Ansh Verma <97027597+anshverma70600@users.noreply.github.com>
2024-02-21 20:19:57 +0000

    string

---
+++ basics/05_string.js
@@ -0,0 +1,14 @@
+const name = "Ansh "
+const age = 20
+// console.log(name + age + " old");
+// but this is not a right way to write
+// we can use backticks for this 
+// console.log(`My name is ${name} and i'm ${age} years old.`);
+
+const newName = new String("Ansh verma")
+const newString = newName
+const newString2 = newName.slice(-10,3)
+
+// console.log(newString);
+// console.log(newString.replace(" ",'-'));
+// console.log(newName);
