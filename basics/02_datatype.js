5e5f5ddc7d1fe75578920a3a9a3faee5ab361668
Ansh Verma <97027597+anshverma70600@users.noreply.github.com>
2024-02-21 18:46:38 +0000

    datatypes done

---
+++ basics/02_datatype.js
@@ -0,0 +1,11 @@
+"use strict" //treat all code as new version of js
+// alert(4+5) node.js support new version , this is not browser
+
+let name = "Ansh"   //String
+let age = 12        //Number
+let male = true     //boolean, null, undefined
+
+//symbol --> unique
+//object 
+
+console.log(typeof age);
