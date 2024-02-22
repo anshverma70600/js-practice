6c96b25da1be007138e65dde30a81f2fdbd5667a
Ansh Verma <97027597+anshverma70600@users.noreply.github.com>
2024-02-22 13:39:37 +0000

    array

---
+++ basics2/01_array.js
@@ -0,0 +1,32 @@
+//Array
+
+//const myArr = [0,1,2,3,4,5,6,7]
+// console.log(myArr[3]);
+
+// push(7)
+//pop() remove last element
+// unshift --> add at begining
+// shift --> reovme from first
+// myarry.includes() --> false
+// indexof(6) --> 6
+// myArry.join() --> String 
+ 
+// Slice and Splicee
+
+// let arry = [2,3,4,7,4,2,5,46,34,432] 
+// console.log(arry.slice(1,6));
+// console.log("A",arry)
+
+// console.log(arry.splice(1,6));
+// console.log("B",arry)
+
+const arr1 = ["My", "name", "is", "known"]
+let arr2 = ["Tata","Tiago"]
+//arr3 = arr1.concat(arr2) it will return new array
+//or push will not workk
+//console.log(arr3); 
+// now we will use spread function
+// const arr3 = [...arr1, ...arr2]
+// console.log(arr3);
+
+console.log(arr1.splice(1,2));
