8bcd054628692dbb37917fa29bdb16902a3f2be7
Ansh Verma <97027597+anshverma70600@users.noreply.github.com>
2024-02-22 14:15:50 +0000

    object

---
+++ basics2/02_object.js
@@ -0,0 +1,10 @@
+const myobj = {
+    name : "ANSH",
+    age : 15,
+    email : "jashkfkubf@gmail.com",
+    city : "Noida"
+}
+
+// console.log(myobj.city);
+// console.log(typeof myobj["age"]);
+
