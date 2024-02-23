6a69cc74ce62c3e8a14e75c23067ef83d85547b9
Ansh Verma <97027597+anshverma70600@users.noreply.github.com>
2024-02-23 20:25:50 +0000

    arrow

---
+++ basics3/03_arrowfun.js
@@ -0,0 +1,16 @@
+// const user = {
+//     name : "Ansh",
+//     age : 12,
+//     message : function(){
+//             console.log(`hello, ${this.name}`);
+//     }
+// }
+// user.message()
+// user.name = "Mac"
+// user.message()
+(function one(){
+    console.log("hello ji one");
+})();
+
+((a) => {console.log(`Hellp ji two, ${a}`)
+})('Ansh Verma');
